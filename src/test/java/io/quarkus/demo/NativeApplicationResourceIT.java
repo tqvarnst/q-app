6574e16e2cc847a7efc7822827127275d06dbd04
Thomas Qvarnström <tqvarnst@redhat.com>
2020-09-11 13:15:28 +0200

    Update the Quarkus version 1.7.3.Final

--- src/test/java/io/quarkus/demo/NativeApplicationResourceIT.java
+++
@@ -1,9 +0,0 @@
-package io.quarkus.demo;
-
-import io.quarkus.test.junit.SubstrateTest;
-
-@SubstrateTest
-public class NativeApplicationResourceIT extends ApplicationResourceTest {
-
-    // Execute the same tests but in native mode.
-}
